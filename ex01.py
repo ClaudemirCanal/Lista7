3dbc82db633919aab07b65fe068450e7099ac131
Claudemir Canal <claudemircanal@hotmail.com>
2025-04-29 20:25:03 -0300

    Primeiro commit

--- ex01.py
+++ ex01.py
@@ -1,11 +1,11 @@
 contador = 1
-while contador <= 10:
+for contador in range (0, 10):
     print("Sou um programa Python!", contador)
     contador += 1
 
 print('\n' + "=" * 30 + '\n')
 
 contador = 1
-while contador <= 10:
+for contador in range (0,10):
     print(contador, "Sou um programa Python!", contador)
     contador += 1
